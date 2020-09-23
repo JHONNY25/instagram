7d6a5ae5182092cba1ba2a9c368aea2ad3ee1e2b
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-09-22 20:44:38 -0600

    create migrations for db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('web_site')->nullable();
+            $table->string('presentation')->nullable();
             $table->rememberToken();
             $table->string('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
