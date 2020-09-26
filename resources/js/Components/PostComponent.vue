472da3ac5a22f99af0cbbbc14f17a76e6d88e046
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-09-25 19:55:00 -0600

    add post component

--- resources/js/Components/PostComponent.vue
+++ resources/js/Components/PostComponent.vue
@@ -1,14 +1,13 @@
 <template>
-    <div class="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl rounded overflow-hidden shadow-none">
+    <div class="bg-white max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl rounded overflow-hidden shadow-none mb-10">
         <header class="flex flex-start">
             <div>
                 <a class="cursor-pointer m-4 flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                    <img class="h-9 w-9 rounded-full object-cover"
-                    src="https://instagram.fsjd1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/117756684_324341175604256_3627028918051777584_n.jpg?_nc_ht=instagram.fsjd1-1.fna.fbcdn.net&_nc_ohc=D_s2gyyHqLMAX9Vf2md&oh=9613db0350b7734e521da0918c56f824&oe=5F8EF6E2"
+                    <img :src="urlImage" class="h-9 w-9 rounded-full object-cover"
                     alt="Jusnito" />
                     <div>
-                        <span class="block ml-2 font-bold">Juanito</span>
-                        <span class="block ml-2 text-xs text-gray-600">5 horas</span>
+                        <span class="block ml-2 font-bold">{{ userName }}</span>
+                        <span class="block ml-2 text-xs text-gray-600">{{ publicationTime }}</span>
                     </div>
                 </a>
             </div>
@@ -19,7 +18,7 @@
 
         <div class="px-6 pt-4">
             <div class="mb-2">
-                <div class="flex items-center">
+                <div class="flex items-center"> 
                     <span class="mr-3 inline-flex items-center cursor-pointer">
                         <svg class="fill-heart text-gray-700 inline-block h-7 w-7 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
@@ -31,25 +30,11 @@
                         </svg>
                     </span>
                 </div>
-                <span class="text-gray-600 text-sm font-bold">2,300 Me gusta</span>
+                <span class="text-gray-600 text-sm font-bold">{{ likes }} Me gusta</span>
             </div>
             <div class="">
-                <div class="text-sm mb-2 flex flex-start" :class="{'relative' : false}">
-                    <div v-if="false" class="absolute top-0 left-0 mr-3">
-                        <a class="cursor-pointer flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                            <img class="h-8 w-8 rounded-full object-cover"
-                            src="https://instagram.fsjd1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/117756684_324341175604256_3627028918051777584_n.jpg?_nc_ht=instagram.fsjd1-1.fna.fbcdn.net&_nc_ohc=D_s2gyyHqLMAX9Vf2md&oh=9613db0350b7734e521da0918c56f824&oe=5F8EF6E2"
-                            alt="Jusnito" />
-                        </a>
-                    </div>
-                    <p class="font-bold" :class="{'ml-12' : false}">
-                        <a class="cursor-pointer">Juanito:</a>
-                        <span class="text-gray-700 font-medium">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                        </span>
-                    </p>
-                </div>
-                <a class="text-gray-400 text-sm cursor-pointer font-semibold">ver los 200 comentarios</a>
+                <comment-post :showUserImage="showUserImage" :comment="comment" :userName="userName" :urlImage="urlImage"></comment-post>
+                <a class="text-gray-400 text-sm cursor-pointer font-semibold" @click="showModal">ver los {{commentsnum}} comentarios</a>
             </div>
 
         </div>
@@ -64,9 +49,23 @@
 </template>
 
 <script>
-
+    import CommentPost from './CommentPost'
+    
     export default {
-        props: ['posts'],
+        data(){
+            return {
+                showUserImage: false
+            }
+        },
+        props: ['userName','likes','commentsnum','comment','urlImage','publicationTime'],
+        components:{
+            CommentPost
+        },
+        methods: {
+            showModal(){
+                this.$emit('show',true)
+            }
+        }
     }
 </script>
 
