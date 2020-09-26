238abf321a63efc2b9901a8e146c64c77fc8670d
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-09-25 19:54:50 -0600

    add component comment post

---
+++ resources/js/Components/CommentPost.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="text-sm mb-2 flex flex-start" :class="{'relative' : showUserImage}">
+        <div v-if="showUserImage" class="absolute top-0 left-0 mr-3">
+            <a class="cursor-pointer flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                <img class="h-8 w-8 rounded-full object-cover"
+                :src="urlImage"
+                alt="Jusnito" />
+            </a>
+        </div>
+        <p class="font-bold" :class="{'ml-12' : showUserImage}">
+            <a class="cursor-pointer">{{userName}}:</a>
+            <span class="text-gray-700 font-medium">
+                {{comment}}
+            </span>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['showUserImage','comment','userName','urlImage']
+    }
+</script>
