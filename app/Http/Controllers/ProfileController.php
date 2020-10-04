f213162b11f58cbd491f68caf7ba101fca21d86f
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-03 21:11:04 -0600

    create profile controller for page profile

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProfileController extends Controller
+{
+    public $user;
+
+    public function __construct(User $user){
+        $this->user = $user;
+    }
+
+    public function index($nick_name){
+        $user = $this->user->where('nick_name',$nick_name)->first();
+
+        return Inertia::render('Profile/Index', [
+            'user' => $user,
+        ]);
+    }
+}
