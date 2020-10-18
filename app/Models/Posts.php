26578847d94b77e6206a9ea6b510be5757b86812
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-17 19:45:38 -0600

    add funtion for add post in model

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use App\Models\Comments;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class Posts extends Model
 {
@@ -26,5 +29,21 @@ class Posts extends Model
 
     public function comments(){
         return $this->hasMany(Comments::class);
+    }
+
+    public static function createPost(Request $request){
+        $file = $request->file('image');
+        $contents = file_get_contents($file);
+        $name = $file->getClientOriginalName();
+
+        if(Storage::disk('s3')->exists($name)){
+            Storage::disk('s3')->put($file, $contents);
+        }
+
+        return (new static)::create([
+            'image_path' => $name,
+            'description' => $request->posttext,
+            'user_id' => Auth::id(),
+        ]);
     }
 }
