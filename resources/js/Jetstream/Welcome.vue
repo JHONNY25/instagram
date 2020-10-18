5d8f500657a27b0a11eb5ec193b729eb472562c0
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-17 19:44:34 -0600

    add html for add post

--- resources/js/Jetstream/Welcome.vue
+++ resources/js/Jetstream/Welcome.vue
@@ -1,10 +1,31 @@
 <template>
-    <div class="overflow-hidden shadow-none sm:rounded-lg max-w-2xl border border-gray-300 mb-10">
-        <post-component @show="changeStateShow" :nickName="nickName" :likes="likes" :commentsnum="comments" :comment="comment" :urlImage="urlImage" :publicationTime="publicationTime"></post-component>
+    <div>
+        <button class="flex justify-center w-full mb-5 text-center bg-blue-500 rounded text-white py-2" @click="changeStateShowPost">Agregar publicación</button>
 
-        <modal :show="show">
-            <div>que pasaaaaaa padreeee</div>
-        </modal>
+        <div class="overflow-hidden shadow-none sm:rounded-lg max-w-2xl border border-gray-300 mb-10">
+            <post-component @show="changeStateShow" :nickName="nickName" :likes="likes" :commentsnum="comments" :comment="comment" :urlImage="urlImage" :publicationTime="publicationTime"></post-component>
+
+            <modal :show="show" @close="changeStateShow">
+                <div class="p-10">que pasaaaaaa padreeee</div>
+            </modal>
+
+            <modal :show="showPost" @close="changeStateShowPost">
+                <div class="p-5">
+                    <div class="border rounded border-gray-300 p-5">
+                        <textarea class="w-full h-16 resize-none outline-none p-2 rounded focus:boutline-none appearance-none" name="posttext" cols="30" rows="10" placeholder="En que estas pensando ...?"></textarea>
+                        <div class="flex justify-end">
+                            <button @click="dispatchInputFile" class="outline-none focus:outline-none mr-3 inline-flex items-center cursor-pointer bg-blue-500 p-2 rounded-full" >
+                                <svg class="text-white inline-block h-7 w-7 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                </svg>
+                            </button>
+                            <input id="image" type="file" name="image" accept="image/gif,image/jpeg,image/jpg,image/png" style="display: none"/>
+                        </div >
+                    </div>
+                    <button class="flex justify-center w-full outline-none focus:outline-none my-3 text-center bg-blue-500 rounded text-white py-2" >Publicar</button>
+                </div>
+            </modal>
+        </div>
     </div>
 </template>
 
@@ -24,19 +45,29 @@
                 publicationTime: '5 horas',
                 comment: 'Un par de jugadas que se dieron en la cancha xd',
                 show: false,
+                showPost: false,
             }
         },
         components: {
             JetApplicationLogo,
             PostComponent,
-            Modal
+            Modal,
         },
         methods:{
             async getPost(){
 
             },
-            changeStateShow(value){
-                this.show = value
+            changeStateShow(){
+                this.show = !this.show
+            },
+            changeStateShowPost(){
+                this.showPost = !this.showPost
+            },
+            dispatchInputFile(){
+                document.getElementById('image').click()
+            },
+            createPost(){
+                
             }
         },
         created(){
