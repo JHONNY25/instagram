da72c68022e6be3a14ddcbd3249a96a5cb0723a0
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-27 19:50:11 -0700

    create userschats

---
+++ resources/js/Components/UsersChats.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul class="overflow-auto" style="height: 500px;">
+        <li>
+            <a href="" class="hover:bg-gray-100 border-b border-gray-300 px-3 py-2 cursor-pointer flex items-center text-sm focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                <img class="h-10 w-10 rounded-full object-cover"
+                src="https://images.pexels.com/photos/5286173/pexels-photo-5286173.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                alt="Juanito" />
+                <div class="w-full pb-2">
+                    <div class="flex justify-between">
+                        <span class="block ml-2 font-semibold text-base text-gray-600 ">Juanito</span>
+                        <span class="block ml-2 text-sm text-gray-600"> 5 hrs</span>
+                    </div>
+                    <span class="block ml-2 text-sm text-gray-600">Ultimo mensaje</span>
+                </div>
+            </a>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+    }
+</script>
