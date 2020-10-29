0efc723cc49e695099ac83fc4eeb6815b92610fd
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-28 19:20:37 -0700

    add function get chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,10 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Chat;
 
 class ChatController extends Controller
 {
     public function index(){
         return Inertia::render('Chat/index');
+    }
+
+    public function getChat($nick_name){
+        if(User::where('nick_name',$nick_name)->exists()){
+            $user = User::where('nick_name',$nick_name)->first();
+    
+            return Chat::with([
+                'userrecive:id,name,profile_photo_path',
+                'usersent:id,name,profile_photo_path',
+                'messages'
+            ])->where('user_recive',$user->id)
+            ->orWhere('user_sent',$user->id)
+            ->first();
+        }else{
+            return response()->json(['error' => 'No se encontro el usuario']);
+        }
     }
 }
