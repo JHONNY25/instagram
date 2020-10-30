2611f3b18b91f5d1af492061569f3a59aa3922b1
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-29 22:17:09 -0700

    add welcome to chats

--- resources/js/Pages/Chat/index.vue
+++ resources/js/Pages/Chat/index.vue
@@ -3,23 +3,31 @@
         <div class="w-10/12">
             <div class="grid grid-cols-3 min-w-full border rounded" style="min-height: 80vh;">
                     <div class="col-span-1 bg-white border-r border-gray-300">
-                        <div>
-                            <h2 class="ml-2 mb-2 text-gray-600 text-lg my-2">Chats</h2>
-                            <div class="my-3 mx-3 ">
-                                <div class="relative text-gray-600 focus-within:text-gray-400">
-                                    <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6 text-gray-500"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                                    </span>
-                                    <input  aria-placeholder="Busca tus amigos o contacta nuevos" placeholder="Busca tus amigos"
-                                    class="py-2 pl-10 block w-full rounded bg-gray-100 outline-none focus:text-gray-700" type="search" name="search" required autocomplete="search" />
-                                </div>
+                        <h2 class="ml-2 mb-2 text-gray-600 text-lg my-2">Chats</h2>
+                        <div class="my-3 mx-3 ">
+                            <div class="relative text-gray-600 focus-within:text-gray-400">
+                                <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6 text-gray-500"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                                </span>
+                                <input  aria-placeholder="Busca tus amigos o contacta nuevos" placeholder="Busca tus amigos"
+                                class="py-2 pl-10 block w-full rounded bg-gray-100 outline-none focus:text-gray-700" type="search" name="search" required autocomplete="search" />
                             </div>
-                            <users-chats @getchat="getChat"></users-chats>
                         </div>
+                        <users-chats @getchat="getChat"></users-chats>
                     </div>
                     <div class="col-span-2 bg-white">
                         <div v-if="chat.length <= 0">
-                            logoooo
+                            <div class="w-full flex items-center justify-center" style="height: 75vh;">
+                                <div class="text-center">
+                                    <div class="border border-gray-600 rounded-full w-24 h-24 relative" style="margin: 0 auto;">
+                                        <svg style="right: 21px; top: 19px;" class="absolute text-gray-600 h-12 w-12 origin-center transform rotate-45" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                                        </svg>
+                                    </div>
+                                    <h5 class="text-gray-600 text-lg my-2">Tus mensajes</h5>
+                                    <span class="block ml-2 text-sm text-gray-600">Envía fotos y mensajes privados a un amigo o un grupo.</span>
+                                </div>
+                            </div>
                         </div>
                         <div v-else>
                             <div class="w-full">
@@ -106,7 +114,7 @@
             UsersChats
         },
         props: [
-            'user',
+            'chats',
         ],
         methods:{
             getChat(){
