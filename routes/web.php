570e9ac6c2d4394c071e8eb6aa09986302cbefb6
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-30 19:59:50 -0700

    change to get

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/profile/{nick_name}', [ProfileController::class,'index'])->name('nick_name');
     Route::get('/users-search/{nick_name}', [SearchController::class,'search'])->name('users-search');
     Route::post('/create-post', [PostController::class,'create'])->name('create-post');
-    Route::post('/get-posts', [PostController::class,'all'])->name('get-posts');
+    Route::get('/get-posts', [PostController::class,'all'])->name('get-posts');
     
     Route::get('/user-chats', [ChatController::class,'index'])->name('get-chats');
     Route::get('/user-chats/{nick_name}', [ChatController::class,'getChat'])->name('get-chat-user');
