eaa25bb566e5437dad85124142e8d64a4d3505dd
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-30 20:01:22 -0700

    remove html and add components

--- resources/js/Pages/Chat/index.vue
+++ resources/js/Pages/Chat/index.vue
@@ -13,12 +13,14 @@
                                 class="py-2 pl-10 block w-full rounded bg-gray-100 outline-none focus:text-gray-700" type="search" name="search" required autocomplete="search" />
                             </div>
                         </div>
-                        
-                        <span v-if="chats.length === 0"class="block text-center my-2 mx-2 text-sm text-gray-600">No tienes conversaciones.</span>
-                        <users-chats v-else @getchat="getChat"></users-chats>
+
+                        <span v-if="chats.length === 0" class="block text-center my-2 mx-2 text-sm text-gray-600">No tienes conversaciones.</span>
+                        <users-chats v-else v-for="(chat,index) in chats" :key="index" :chatid="chat.id" :lastmessage="chat.messages[0].text" 
+                        :messagedate="chat.messages[0].send_date" :username="chat.userrecive.id === $page.user.id ? chat.usersent.nick_name : chat.userrecive.nick_name" 
+                        :userimage="chat.userrecive.id === $page.user.id ? $chat.usersent.profile_photo_url : chat.userrecive.profile_photo_url " @getchat="getChat"></users-chats>
                     </div>
                     <div class="col-span-2 bg-white">
-                        <div v-if="chat.length <= 0" class="w-full flex items-center justify-center" style="height: 75vh;">
+                        <div v-if="userchat.length <= 0" class="w-full flex items-center justify-center" style="height: 75vh;">
                             <div class="text-center">
                                 <div class="border border-gray-600 rounded-full w-24 h-24 relative" style="margin: 0 auto;">
                                     <svg style="right: 21px; top: 19px;" class="absolute text-gray-600 h-12 w-12 origin-center transform rotate-45" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -29,67 +31,10 @@
                                 <span class="block ml-2 text-sm text-gray-600">Envía fotos y mensajes privados a un amigo o un grupo.</span>
                             </div>
                         </div>
-                        <div v-else class="w-full">
-                            <div class="flex items-center border-b border-gray-300 pl-3 py-3">
-                                <img class="h-10 w-10 rounded-full object-cover"
-                                src="https://images.pexels.com/photos/5286173/pexels-photo-5286173.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-                                alt="Juanito" />
-                                <span class="block ml-2 font-bold text-base text-gray-600">Juanito</span>
-                            </div>
-                            <div class="w-full overflow-y-auto p-10" style="height: 700px;">
-                                <ul>
-                                    <!--<div class="w-full flex text-center">
-                                        <div class="w-full px-5 py-2 my-2 text-white text-center text-3xl">
-                                            No existe una conversación, se el primero en conversar!!
-                                        </div>
-                                    </div> -->
-                                    <li class="clearfix2">
-                                        <div class="w-full flex justify-end">
-                                            <div class="bg-gray-100 rounded px-5 py-2 my-2 text-gray-700 relative" style="max-width: 300px;">
-                                                Probandooo
-                                                <div class="absolute w-0 h-0"
-                                                style="border-bottom: 15px solid transparent;
-                                                    right: -25px;
-                                                    border-left: 15px solid #f4f5f7;
-                                                    border-right: 15px solid transparent;
-                                                    top: 0;"></div>
-                                            </div>
-                                        </div>
-                                    </li>
 
-                                    <li class="clearfix2">
-                                        <div class="w-full flex justify-start">
-                                            <div class="bg-gray-100 rounded px-5 py-2 my-2 text-gray-700 relative" style="max-width: 300px;">
-                                                Probandooo
-                                                <div class="absolute w-0 h-0"
-                                                style="border-bottom: 15px solid transparent;
-                                                    left: -25px;
-                                                    border-left: 15px solid transparent;
-                                                    border-right: 15px solid #f4f5f7;
-                                                    top: 0;"></div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div class="w-full py-3 px-3 flex items-center justify-between border-t border-gray-300">
-                                <button class="outline-none focus:outline-none">
-                                    <svg class="text-gray-400 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
-                                    </svg>
-                                </button>
-                                <input aria-placeholder="Escribe un mensaje aquí" placeholder="Escribe un mensaje aquí"
-                                    class="py-2 mx-3 pl-5 block w-full rounded-full bg-gray-100 outline-none focus:text-gray-700" type="text" name="message" required/>
-
-                                <button class="outline-none focus:outline-none">
-                                    <svg class="text-gray-400 h-7 w-7 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-                                    </svg>
-                                </button>
-                            </div>
-
-                        </div>
+                        <chat v-else :userimage="userchat.userrecive.id === $page.user.id ? userchat.usersent.profile_photo_url : userchat.userrecive.profile_photo_url"
+                        :username="userchat.userrecive.id === $page.user.id ? userchat.usersent.nick_name : userchat.userrecive.name"
+                        :messages="userchat.messages"></chat>
                     </div>
                 </div>
             </div>
@@ -100,23 +45,29 @@
 <script>
     import AppLayout from '../../Layouts/AppLayout'
     import UsersChats from '../../Components/UsersChats'
+    import Chat from './Chat'
 
     export default {
         data(){
             return {
-                chat: []
+                userchat: [],
             }
         },
         components: {
             AppLayout,
-            UsersChats
+            UsersChats,
+            Chat
         },
         props: [
             'chats',
         ],
         methods:{
-            getChat(){
-                this.chat.push({user:'sadasd'})
+            async getChat(username){
+                await axios.get('/user-chats/'+username)
+                .then(response => {
+                    this.userchat = response.data
+                })
+                .catch(error => console.log(error))
             }
         }
     }
