cd2ed1768ec7087ee6d710adc392415bf25ec11e
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-11-05 20:24:11 -0700

    add data message and conditions, v-model

--- resources/js/Pages/Chat/Chat.vue
+++ resources/js/Pages/Chat/Chat.vue
@@ -8,11 +8,11 @@
         </div>
         <div class="w-full overflow-y-auto p-10" style="height: 700px;">
             <ul>
-                <!--<div class="w-full flex text-center">
+                <div v-if="messages.lenght <= 0" class="w-full flex text-center">
                     <div class="w-full px-5 py-2 my-2 text-white text-center text-3xl">
                         No existe una conversación, se el primero en conversar!!
                     </div>
-                </div> -->
+                </div>
                 <li v-for="(message,index) in messages" :key="index" class="clearfix2">
                     <div class="w-full flex" :class="[message.user_id === usercurrent ? 'justify-end' : 'justify-start']">
                         <div class="bg-gray-100 rounded px-5 py-2 my-2 text-gray-700 relative" style="max-width: 300px;">
@@ -33,10 +33,10 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
                 </svg>
             </button>
-            <input aria-placeholder="Escribe un mensaje aquí" placeholder="Escribe un mensaje aquí"
+            <input v-model="message" aria-placeholder="Escribe un mensaje aquí" placeholder="Escribe un mensaje aquí"
                 class="py-2 mx-3 pl-5 block w-full rounded-full bg-gray-100 outline-none focus:text-gray-700" type="text" name="message" required/>
 
-            <button class="outline-none focus:outline-none" @click="$emit('sendmessage',username)">
+            <button v-if="message !== ''" class="outline-none focus:outline-none" @click="$emit('sendmessage',username)">
                 <svg class="text-gray-400 h-7 w-7 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
                 </svg>
@@ -48,6 +48,11 @@
 
 <script>
     export default {
+        data(){
+            return{
+                message: ''
+            }
+        },
         props:[
             'username','userimage','messages','usercurrent'
         ],
