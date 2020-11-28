a3857b314510e69cec170bfaa5faadbe76522f8d
JHONNY25 <jonas97isk@hotmail.com>
2020-11-27 20:42:59 -0700

    add conditions for likes, and add function for like or dislike

--- resources/js/Components/PostComponent.vue
+++ resources/js/Components/PostComponent.vue
@@ -2,12 +2,12 @@
     <div class="bg-white max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl rounded overflow-hidden shadow-none mb-10 sm:rounded-lg max-w-2xl border border-gray-300">
         <header class="flex flex-start">
             <div>
-                <a :href="'/profile/'+nickName" class="cursor-pointer m-4 flex items-center text-sm outline-none focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                    <img :src="urlImage" class="h-9 w-9 rounded-full object-cover"
-                    alt="Jusnito" />
+                <a :href="'/profile/'+post.user.nick_name" class="cursor-pointer m-4 flex items-center text-sm outline-none focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                    <img :src="post.user.profile_photo_url" class="h-9 w-9 rounded-full object-cover"
+                    :alt="post.user.nick_name" />
                     <div>
-                        <h2 class="block ml-2 font-bold">{{ nickName }}</h2>
-                        <span class="block ml-2 text-xs text-gray-600">{{ getDifferenceTime(publicationTime) }}</span>
+                        <h2 class="block ml-2 font-bold">{{ post.user.nick_name }}</h2>
+                        <span class="block ml-2 text-xs text-gray-600">{{ getDifferenceTime(post.created_at) }}</span>
                     </div>
                 </a>
             </div>
@@ -19,8 +19,13 @@
         <div class="px-6 pt-4">
             <div class="mb-2">
                 <div class="flex items-center">
-                    <span class="mr-3 inline-flex items-center cursor-pointer">
-                        <svg class="fill-heart text-gray-700 inline-block h-7 w-7 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <span v-if="post.likes.find(u => u.user_id === $page.user.id)" @click="likeOrDislike" class="mr-3 inline-flex items-center cursor-pointer">
+                        <svg class="fill-heart text-gray-700 inline-block h-7 w-7 heart" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                        </svg>
+                    </span>
+                    <span v-else @click="likeOrDislike" class="mr-3 inline-flex items-center cursor-pointer">
+                        <svg class="fill-heart text-gray-700 inline-block h-7 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                         </svg>
                     </span>
@@ -30,11 +35,11 @@
                         </svg>
                     </span>
                 </div>
-                <span class="text-gray-600 text-sm font-bold">{{ likes }} Me gusta</span>
+                <span class="text-gray-600 text-sm font-bold">{{ post.countlikes }} Me gusta</span>
             </div>
             <div class="">
-                <comment-post :showUserImage="showUserImage" :comment="comment" :nickName="nickName" :urlImage="urlImage"></comment-post>
-                <a class="text-gray-400 text-sm cursor-pointer font-semibold" @click="showModal">ver los {{commentsnum}} comentarios</a>
+                <comment-post :showUserImage="showUserImage" :comment="post.description" :nickName="post.user.nick_name" :urlImage="post.user.profile_photo_url"></comment-post>
+                <a class="text-gray-400 text-sm cursor-pointer font-semibold" @click="showModal">ver los {{post.countcomments}} comentarios</a>
             </div>
 
         </div>
@@ -55,10 +60,10 @@
     export default {
         data(){
             return {
-                showUserImage: false
+                showUserImage: false,
             }
         },
-        props: ['nickName','likes','commentsnum','comment','urlImage','publicationTime'],
+        props: ['post'],
         components:{
             CommentPost
         },
@@ -68,6 +73,21 @@
             },
             getDifferenceTime(date){
                 return moment(date).toNow(true);
+            },
+            async likeOrDislike(){
+                const formData = new FormData()
+                formData.append("postId", this.post.id)
+
+                await axios.post('/like-post',formData)
+                .then(response => {
+                    this.post.likes = response.data.likes
+                    if(response.data.like){
+                        this.post.countlikes++
+                    }else{
+                        --this.post.countlikes
+                    }
+                })
+                .catch(error => console.log(error))
             }
         }
     }
@@ -75,6 +95,11 @@
 
 <style>
     .fill-heart:hover{
+        fill: #e53e3e;
+        color: #e53e3e;
+    }
+
+    .heart{
         fill: #e53e3e;
         color: #e53e3e;
     }
