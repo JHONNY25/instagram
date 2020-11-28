422a7c6ed087a71793673d28ea87c41af96bcf17
JHONNY25 <jonas97isk@hotmail.com>
2020-11-27 20:41:27 -0700

    add create like or remove like in like model

--- app/Models/Likes.php
+++ app/Models/Likes.php
@@ -22,4 +22,16 @@ class Likes extends Model
     public function posts(){
         return $this->belongsTo(Posts::class,'post_id');
     }
+
+    public static function createLike(int $postId, int $userId){
+        return (new static)::create([
+            'like' => 1,
+            'post_id' => $postId,
+            'user_id' => $userId,
+        ]);
+    }
+
+    public static function deleteLike(int $postId, int $userId){
+        (new static)::where('post_id',$postId)->where('user_id',$userId)->delete();
+    }
 }
