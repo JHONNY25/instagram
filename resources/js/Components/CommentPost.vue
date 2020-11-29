1087c002e515c1e9ef56424e20f0953fb6b6ff8d
JHONNY25 <jonas97isk@hotmail.com>
2020-11-28 19:48:20 -0700

    change class css

--- resources/js/Components/CommentPost.vue
+++ resources/js/Components/CommentPost.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="text-sm mb-2 flex flex-start" :class="{'relative' : showUserImage}">
-        <div v-if="showUserImage" class="absolute top-0 left-0 mr-3">
+    <div class="text-sm mb-2 flex flex-start items-center">
+        <div v-if="showUserImage">
             <a :href="'/'+nickName" class="cursor-pointer flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
                 <img class="h-8 w-8 rounded-full object-cover"
                 :src="urlImage"
-                alt="Jusnito" />
+                :alt="nickName" />
             </a>
         </div>
-        <p class="font-bold" :class="{'ml-12' : showUserImage}">
+        <p class="font-bold" :class="{'ml-2' : showUserImage}">
             <a class="cursor-pointer">{{nickName}}:</a>
-            <span class="text-gray-700 font-medium">
+            <span class="text-gray-700 font-medium ml-1">
                 {{comment}}
             </span>
         </p>
