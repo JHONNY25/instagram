2f35318f1d594202ad15ea59602ca6c9b9b8fd93
JHONNY25 <jonas97isk@hotmail.com>
2020-12-10 19:26:01 -0700

    change props, add conditions when not exists messages

--- resources/js/Components/UsersChats.vue
+++ resources/js/Components/UsersChats.vue
@@ -1,14 +1,14 @@
 <template>
-    <a @click="$emit('getchat',username)" class="hover:bg-gray-100 border-b border-gray-300 px-3 py-2 cursor-pointer flex items-center text-sm focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+    <a @click="emmitEvent" class="hover:bg-gray-100 border-b border-gray-300 px-3 py-2 cursor-pointer flex items-center text-sm focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
         <img class="h-10 w-10 rounded-full object-cover"
         :src="userimage"
         :alt="username" />
         <div class="w-full pb-2">
             <div class="flex justify-between">
                 <span class="block ml-2 font-semibold text-base text-gray-600 ">{{ username }}</span>
-                <span class="block ml-2 text-sm text-gray-600"> {{ messagedate ? 'Hace '+getDifferenceTime(messagedate) : '' }}</span>
+                <span v-if="message.length > 0" class="block ml-2 text-sm text-gray-600"> {{ 'Hace '+getDifferenceTime(message[0].send_date) }}</span>
             </div>
-            <span class="block ml-2 text-sm text-gray-600">{{ lastmessage }}</span>
+            <span v-if="message.length > 0" class="block ml-2 text-sm text-gray-600">{{ message[0].text }}</span>
         </div>
     </a>
 </template>
@@ -18,17 +18,21 @@
 
     export default {
         props:{
-            lastmessage: {
-                type: String,
-                required: false
-            },
-            messagedate: {
-                type: String,
+            message: {
+                type: Array,
                 required: false
             },
             username:{
                 type: String,
                 required: true
+            },
+            chatid:{
+                type: Number,
+                required: false
+            },
+            userid:{
+                type: Number,
+                required: false
             },
             userimage:{
                 type: String,
@@ -38,6 +42,13 @@
         methods:{
             getDifferenceTime(date){
                 return moment(date).toNow(true);
+            },
+            emmitEvent(){
+                if(this.chatid){
+                    this.$emit('getChat',this.chatid)
+                }else{
+                    this.$emit('getNewChat',this.userid)
+                }
             }
         }
     }
