66996febcd3ded6683dc47c772519ed547472107
JHONNY25 <jonas97isk@hotmail.com>
2020-12-15 18:54:49 -0700

    add inertia progress

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@ import { InertiaForm } from 'laravel-jetstream';
 import PortalVue from 'portal-vue';
 import VueMoment from 'vue-moment'
 import moment from 'moment-timezone'
+import { InertiaProgress } from '@inertiajs/progress'
+import NProgress from 'nprogress'
 
 moment.locale('es');
 
@@ -17,6 +19,21 @@ Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
 const app = document.getElementById('app');
+
+InertiaProgress.init({
+    // The delay after which the progress bar will
+    // appear during navigation, in milliseconds.
+    delay: 500,
+  
+    // The color of the progress bar.
+    color: '#1c64f2',
+  
+    // Whether to include the default NProgress styles.
+    includeCSS: true,
+  
+    // Whether the NProgress spinner will be shown.
+    showSpinner: true,
+})
 
 new Vue({
     render: (h) =>
