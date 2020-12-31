b1cbeed6944e528b147542e43953b53543dd7a6b
JHONNY25 <jonas97isk@hotmail.com>
2020-12-30 18:47:37 -0700

    add function for mark as read all notifications

--- resources/js/Components/Notification.vue
+++ resources/js/Components/Notification.vue
@@ -2,9 +2,9 @@
     <div class="">
         <dropdown align="right" width="80" overflow="overflow-y-auto" maxheight="300">
             <template #trigger>
-                <div class="mr-3 relative">
-                    <div class="bg-red-500 w-2 h-2 text-xs absolute top-0 right-0 rounded-full"></div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 cursor-pointer">
+                <div @click="markAsReadNotification" class="mr-3 relative">
+                    <div v-if="$page.unreadNotificationsCount > 0" class="cursor-pointer flex items-center justify-center bg-red-500 w-4 h-4 absolute top-0 right-0 rounded-full"><span class="text-xs text-white">{{ $page.unreadNotificationsCount > 9 ? '9+' : $page.unreadNotificationsCount }}</span></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-7 h-7 cursor-pointer">
                         <path class="text-gray-600" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                     </svg>
                 </div>
@@ -15,12 +15,15 @@
                     <img :src="unread.data.user.profile_photo_url"
                         class="w-9 h-9 object-cover rounded-full"
                         :alt="unread.data.user.name" />
-                    <div class="ml-2">
+                    <div class="ml-2 w-full">
                         <h1 class="block leading-relaxed font-bold text-gray-700 text-sm"> {{ unread.data.user.nick_name }}</h1>
-                        <div class="text-sm font-light text-gray-600">{{ unread.data.message + ' ' +  getDifferenceTime(unread.data.created_at) }}</div>
+                        <div class="text-sm font-light text-gray-600 flex justify-between">
+                            <div>{{ unread.data.message }}</div>
+                            <div>{{ getDifferenceTime(unread.created_at) }}</div>
+                        </div>
                     </div>
                 </a>
-                <div v-else class="flex items-center py-2 px-3">
+                <div v-if="unreads.length === 0" class="flex items-center py-2 px-3">
                     <h2 class="text-sm font-light text-gray-400">No tienes notificaciones</h2>
                 </div>
             </template>
@@ -34,11 +37,6 @@
     import moment from 'moment'
 
     export default {
-        data(){
-            return {
-                notifications: this.unreads
-            }
-        },
         props:['unreads','userId'],
         components:{
             Dropdown
@@ -47,12 +45,13 @@
             getDifferenceTime(date){
                 return moment(date).toNow(true);
             },
-        },
-        mounted(){
-            Echo.private('App.Models.User.' + this.userId)
-                .notification((notification) => {
-                    console.log(notification);
-                });
+            async markAsReadNotification(){
+                await axios.post('/mark-as-read-notifications')
+                    .then((response) => {
+                        this.$page.unreadNotificationsCount = 0
+                    })
+                    .catch(error => console.log(error))
+            }
         }
     }
 </script>
