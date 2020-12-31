4d5541dcc7558004d9fdc78c1f043b1c7a376032
JHONNY25 <jonas97isk@hotmail.com>
2020-12-30 18:44:44 -0700

    add method for mark as read notifications

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -8,7 +8,13 @@ use App\Models\User;
 class NotificationsController extends Controller
 {
      public function getUnreadNotification(){
-         return auth()->user()->unreadNotifications;
+         return auth()->user()->notifications;
      }
 
+     public function markAsRead(){
+        $user = auth()->user();
+        $user->unreadNotifications->markAsRead();
+
+        return $user->notifications;
+    }
 }
