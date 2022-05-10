d24a6f02fb655a39e93f25e8bd73c7c915b78b8d
Jonathan Rodríguez <jonas97isk@hotmail.com>
2022-05-09 22:49:11 -0600

    Update HandleInertiaRequests.php

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            'unreadNotificationsCount' => auth()->user()->unreadNotifications()->count()
+            'unreadNotificationsCount' => auth()->check() ? auth()->user()->unreadNotifications()->count() : 0
         ]);
     }
 }
